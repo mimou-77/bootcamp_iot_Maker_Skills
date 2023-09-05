71ad5a7e4cefed118de9f8840028b290818e0565
mimou-77 <mariem.benhassine79@gmail.com>
2023-09-05 14:17:45 +0100

    update example 5 de nouveau

--- bootcamp_iot_section_1_examples_of_simple_arduino_systems/example_5_serial_monitor+potentiometer/serial_monitor-potentiometer/serial_monitor-potentiometer.ino
+++ bootcamp_iot_section_1_examples_of_simple_arduino_systems/example_5_serial_monitor+potentiometer/serial_monitor-potentiometer/serial_monitor-potentiometer.ino
@@ -1,28 +1,19 @@
-#include "Arduino.h"  // we need to include the "Arduino.h" library to be able to use the serial monitor methods.
+#include "Arduino.h"  // we need to include the "Arduino.h" library to be able to use the serial monitor methods
 
-#define ssid "XX"     // replace "---" with the ssid of your network
-#define password "YY"  // replace "---" with the password of your network
+#define potPin 36 // we verify that the pin 36 is connected to the ADC ( we need to read an analog signal from the potentiometer)
+
+int potValue=0; // the value of the signal we recieve from the potentiometer (analog signal converted to digital via the ADC)
 
 void setup() {
 
-  Serial.begin(115200);      // we establish a serial connection with the computer 
-  WiFi.begin(ssid,password); // we establish a wifi connection to the chosen network defined by the (ssid,password)
-
-  
-  /* waits for ESP to connect to the wifi network. Prints a dot each second while the ESP isn't connected to a Wifi network. When the ESP connects, prints "wifi conncted" */
-  Serial.println("Trying to connect to Wifi");
-  while (WiFi.status() != WL_CONNECTED) {
-  Serial.print(".");
-  delay(1000);
-  }
-  Serial.println("\n connected to Wifi");
-
-  /* Displays the local Ip of the ESP on the serial monitor. Local IP = IP of the ESP32 in the local network it is connected to */
-  Serial.print("local IP: ");
-  Serial.print(WiFi.localIP());
+  Serial.begin(115200); // we establish a serial connection with a baudrate of 115200 (the baudrate required by an ESP32 board)
 }
 
 void loop() {
-  //nothing to be executed on a loop
+
+  /*displays each 5 seconds the value of the signal received from the potentiometer*/
+  potValue=analogRead(potPin); // reads the signal received from the potentiometer
+  Serial.println(potValue); // displays the value of the signal received from the potentiometer
+  delay(5000); // waits for 5s 
 }
 
